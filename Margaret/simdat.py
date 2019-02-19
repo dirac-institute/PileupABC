a8667ba7d7c7052d8577b18f75e7eee46f5213ef
Margaret Lazzarini <margaret.lazzarini@gmail.com>
2019-02-19 14:52:54 -0800

    adding abc work in progress

--- Margaret/simdat.py
+++ Margaret/simdat.py
@@ -35,7 +35,7 @@ def energies_rs(a,b,gamma,N):
                 if (k<=p_r):
                     photons[count] = r
                     count += 1
-    print("Time to generate photon energies = %s seconds" % (time.time() - start_time))
+    #print("Time to generate photon energies = %s seconds" % (time.time() - start_time))
     return photons
 
 def energies_cdf(a,b,gamma,N):
@@ -49,35 +49,35 @@ def energies_cdf(a,b,gamma,N):
     start_time = time.time()
     r = np.random.uniform(0,1,N)
     x = a*(1-r)**(-1/(gamma-1))
-    print("Time to generate photon energies = %s seconds" % (time.time() - start_time))
+    #print("Time to generate photon energies = %s seconds" % (time.time() - start_time))
     return x
 
-def simulate_data(stop,handle):
+def simulate_data(stop,gamma,handle,arf_bins):
     ''' Generates an un-piled and piled list of photon energies. 
         stop = end time of observation, generally equals total length of obs in seconds.
+        gamma = photon index
         handle = 'rs' to generate photons via rejection 
     '''
     #Assign physical parameters
     start = 0. #s
     #stop = 100 #s
     cr = 1. #count rate between 0.1 and 10 photons/second
-    gamma = 2.7 #assume photon index
     read_time = 3.2 #s
     energy_lo = 0.1 #keV
     energy_hi = 10.0 #keV
     K = (stop-start)*cr #Number of photons to generate
     N = np.random.poisson(K) #choose a number of photons from a poisson distribution centered on expected, K
-    print 'number of photons observed =', N
+    #print 'number of photons observed =', N
     
     #Assign photon arrival times, random within obervation time
     arrival_times = np.random.uniform(start,stop,N)
     
     #Assign photon energies, random with a power law distribution.
     if handle == 'rs':
-        print 'generating photon energies via rejection sampling'
+        #print 'generating photon energies via rejection sampling'
         energy = energies_rs(energy_lo,energy_hi,gamma,N) #generate photon energies using rejection sampling
     if handle == 'cdf':
-        print 'generating photon energies via CDF method'
+        #print 'generating photon energies via CDF method'
         energy = energies_cdf(energy_lo,energy_hi,gamma,N)
     else:
         print 'invalid photon generation method entered'
@@ -95,29 +95,37 @@ def simulate_data(stop,handle):
     for i in range(len(time_hist)):
         if time_hist[i] > 1:
             #print time_hist[i]
-            num_piled += time_hist[i]
+            num_piled += time_hist[i]-1
             first_photon = np.sum(time_hist[0:i])
             last_photon = first_photon + time_hist[i] - 1
             sum_energy = np.sum(energies[first_photon:last_photon])
             piled_energy[first_photon] = sum_energy
             piled_energy[first_photon+1:last_photon] = 0.0
-    print 'Fraction of photons that are piled =', np.float(num_piled) / N
-    return energy, piled_energy
+    #print 'Fraction of photons that are piled (# photons lost) =', np.float(num_piled) / N
+    energy = np.array(filter((lambda x: np.isinf(x)==False),energy))
+    piled_energy = np.array(filter((lambda x: np.isinf(x)==False),piled_energy))
+    #print 'energy',energy
+    #print 'piled energy',piled_energy
+    #print 'piled energy > 0',piled_energy[piled_energy>0]
+    e_hist, e_bins = np.histogram(energy,bins=arf_bins)
+    pe_hist, pe_bins = np.histogram(piled_energy[piled_energy>0],bins=arf_bins)
+    #print pe_hist
+    return e_bins[1:], e_hist, pe_hist
 
-def plot_data(raw,piled):
+def plot_data(e_bins,e_hist,pe_hist):
     '''
     Takes output data from simulate_data() and plots the raw vs. piled spectra
+    Inputs:
+    e_bins = energy bins from the ARF
+    e_hist = number of photons in each energy bin for the unpiled spectrum
+    pe_hist = number of photons in each energy bin for the piled spectrum
     '''
-    arf = Table.read('/Users/mlazz/Dropbox/UW/PileupABC/13858/repro/SDSSJ091449.05+085321.corr.arf',format='fits')
     fig,ax=plt.subplots(figsize=(8,5))
-    #num=len(raw)/50.
-    e_hist, e_bins = np.histogram(raw,bins=arf['ENERG_HI'])
-    pe_hist, pe_bins = np.histogram(piled[piled>0],bins=arf['ENERG_HI'])
-    ax.plot(e_bins[1:],e_hist,label='un-piled')
-    ax.plot(pe_bins[1:],pe_hist,label='piled')
+    ax.plot(e_bins,e_hist,label='un-piled')
+    ax.plot(e_bins,pe_hist,label='piled')
     ax.set_xlabel('Energy (keV)')
     ax.set_xlim(-0.5,11)
     ax.set_ylabel('Flux, unscaled')
     ax.set_yscale('log')
     ax.legend()
-    return e_hist, pe_hist
+    #return e_hist, pe_hist
